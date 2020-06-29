ca9882e30b4e3abfdc4d5e6466e3b80348568a88
Federico Mazzanti <f.mazzanti@vargroup.it>
2020-06-30 01:42:15 +0200

    upgraded

--- src/VimeoClient/Common/VimeoAPIInformation.cs
+++ src/VimeoClient/Common/VimeoAPIInformation.cs
@@ -1,4 +1,7 @@
-﻿namespace VimeoClient.Common
+﻿using RestClient.Generic;
+using VimeoClient.Response;
+
+namespace VimeoClient.Common
 {
     /// <summary>
     /// 
@@ -7,5 +10,13 @@
     {
         public VimeoAPIInformation(VimeoProperties properties)
                : base(properties) { }
+
+
+        /// <summary>
+        /// This method returns the full OpenAPI specification for the Vimeo API.
+        /// </summary>
+        /// <returns></returns>
+        public RestResult<APIApp> GetTheAPISpecification() => RootAuthorization()
+            .Get<APIApp>();
     }
 }
--- src/VimeoClient/Responses/APIApp.cs
+++ src/VimeoClient/Responses/APIApp.cs
@@ -1,5 +1,21 @@
 ﻿namespace VimeoClient.Response
 {
-    public class APIApp {  }
+    public class APIApp
+    {
+        public string openapi { get; set; }
+        public Info info { get; set; }
+    }
 
+    public class Info
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string version { get; set; }
+        public Contact contact { get; set; }
+    }
+
+    public class Contact
+    {
+        public string url { get; set; }
+    }
 }
