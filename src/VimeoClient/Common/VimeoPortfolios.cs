6b294e195dee871680c1dbdb206ad5fca2090377
Federico Mazzanti <f.mazzanti@vargroup.it>
2020-07-04 08:52:57 +0200

    VimeoShowCases & Vimeo Portfolios

--- src/VimeoClient/Common/VimeoPortfolios.cs
+++ src/VimeoClient/Common/VimeoPortfolios.cs
@@ -1,24 +1,56 @@
-﻿using RestClient;
+﻿/// <summary>
+/// 
+/// The MIT License (MIT)
+/// 
+/// Copyright (c) 2020 Federico Mazzanti
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// </summary>
 
 namespace VimeoClient.Common
 {
+    using RestClient;
+
     /// <summary>
-    /// 
+    /// Portfolios are customizable websites for showcasing videos.
+    /// Vimeo Pro, Business, and Premium subscribers have access to this feature.For more information, see our Help Center.
+    /// https://developer.vimeo.com/api/reference/portfolios
     /// </summary>
     public class VimeoPortfolios
     {
         /// <summary>
-        /// 
+        /// Properties
         /// </summary>
         public VimeoProperties Properties { get; private set; }
 
         /// <summary>
-        /// 
+        /// Root Authorization
         /// </summary>
         public RestBuilder RootAuthorization { get; private set; }
 
         /// <summary>
-        /// 
+        /// Portfolios are customizable websites for showcasing videos. 
+        /// Vimeo Pro, Business, and Premium subscribers have access to this feature.For more information, see our Help Center.
         /// </summary>
         /// <param name="properties"></param>
         /// <param name="rootAuthorization"></param>
@@ -27,5 +59,17 @@ namespace VimeoClient.Common
             this.Properties = properties;
             this.RootAuthorization = rootAuthorization;
         }
+
+        #region [ Essentials ]
+        //Get a specific portfolio
+        //Get all the portfolios that belong to the user
+        #endregion
+
+        #region [ Videos ]
+        //Add a video to a portfolio
+        //Get a specific video in a portfolio
+        //Get all the videos in a portfolio
+        //Remove a video from a portfolio
+        #endregion
     }
 }
--- src/VimeoClient/Common/VimeoShowCases.cs
+++ src/VimeoClient/Common/VimeoShowCases.cs
@@ -1,24 +1,56 @@
-﻿using RestClient;
+﻿/// <summary>
+/// 
+/// The MIT License (MIT)
+/// 
+/// Copyright (c) 2020 Federico Mazzanti
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// </summary>
 
 namespace VimeoClient.Common
 {
+    using RestClient;
+
     /// <summary>
-    /// 
+    /// A showcase (previously album) is a collection of videos for public or private sharing. 
+    /// When developing for this feature, keep in mind that our endpoint syntax uses the original album nomenclature, although this is subject to change in the future. 
+    /// For more information about showcases, see our Help Center.
+    /// https://developer.vimeo.com/api/reference/showcases
     /// </summary>
     public class VimeoShowCases
     {
         /// <summary>
-        /// 
+        /// Properties
         /// </summary>
         public VimeoProperties Properties { get; private set; }
 
         /// <summary>
-        /// 
+        /// Root Authorization
         /// </summary>
         public RestBuilder RootAuthorization { get; private set; }
 
         /// <summary>
-        /// 
+        /// A showcase (previously album) is a collection of videos for public or private sharing. 
         /// </summary>
         /// <param name="properties"></param>
         /// <param name="rootAuthorization"></param>
@@ -27,5 +59,41 @@ namespace VimeoClient.Common
             this.Properties = properties;
             this.RootAuthorization = rootAuthorization;
         }
+
+        #region [ Essentials ]
+        //Create a showcase
+        //Delete a showcase
+        //Edit a showcase
+        //Get a specific showcase
+        //Get all the showcases that belong to the user
+        #endregion
+
+        #region [  Custom showcase logos ]
+        //Add a custom logo to a showcase
+        //Delete a custom showcase logo
+        //Get a specific custom showcase logo
+        //Get all the custom logos of a showcase
+        //Replace a custom showcase logo
+        #endregion
+
+        #region [ Custom showcase thumbnails ]
+        //Add a custom thumbnail to a showcase
+        //Delete a custom showcase thumbnail
+        //Get a specific custom showcase thumbnail
+        //Get all the custom thumbnails of a showcase
+        //Replace a custom showcase thumbnail
+        #endregion
+
+        #region [  Showcase videos ]
+        //Add a specific video to a showcase
+        //Create a thumbnail for a showcase from a showcase video
+        //Get a specific video in a showcase
+        //Get all the showcases to which the user can add or remove a specific video
+        //Get all the user's videos that can be added to or removed from a showcase
+        //Get all the videos in a showcase
+        //Remove a video from a showcase
+        //Replace all the videos in a showcase
+        //Set the featured video of a showcase
+        #endregion
     }
 }
