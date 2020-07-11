590885c9a130fe1d5ae605b3e424848423b37311
Federico Mazzanti <f.mazzanti@vargroup.it>
2020-07-11 08:48:14 +0200

    EmbedPresets Essential

--- src/VimeoClient/Common/VimeoEmbedPresets.cs
+++ src/VimeoClient/Common/VimeoEmbedPresets.cs
@@ -32,6 +32,7 @@ namespace VimeoClient.Common
     using RestClient;
     using RestClient.Generic;
     using VimeoClient.Model;
+    using VimeoClient.Response;
 
     /// <summary>
     /// An embed preset is a reusable collection of settings for customizing the appearance and behavior of the embeddable Vimeo player. 
@@ -112,9 +113,100 @@ namespace VimeoClient.Common
             })
             .Patch<Presets>();
 
+        /// <summary>
+        /// This method returns a single embed preset. The authenticated user must be the owner of the preset.
+        /// </summary>
+        /// <param name="preset_id">The ID of the preset.</param>
+        /// <param name="user_id">The ID of the user.</param>
+        /// <returns></returns>
+        public RestResult<Presets> GetASpecificEmbedPreset(int preset_id, int user_id) => RootAuthorization()
+            .Command($"/users/{user_id}/presets/{preset_id}")
+            .Get<Presets>();
 
-        //Get a specific embed preset
-        //Get all the embed presets that a user has created
+        /// <summary>
+        /// This method returns a single embed preset. The authenticated user must be the owner of the preset.
+        /// </summary>
+        /// <param name="preset_id">The ID of the preset.</param>
+        /// <returns></returns>
+        public RestResult<Presets> GetASpecificEmbedPreset(int preset_id) => RootAuthorization()
+            .Command($"/me/presets/{preset_id}")
+            .Get<Presets>();
+
+        /// <summary>
+        /// This method returns every embed preset that belongs to the authenticated user.
+        /// </summary>
+        /// <param name="user_id">The ID of the user.</param>
+        /// <param name="page">The page number of the results to show.</param>
+        /// <param name="per_page">The number of items to show on each page of results, up to a maximum of 100.</param>
+        /// <returns></returns>
+        public RestResult<Pagination<Presets>> GetAllTheEmbedPresetsThatAUserHasCreadted(int user_id, int? page = null, int? per_page = null)
+        {
+            var root = RootAuthorization()
+                .Command($"/users/{user_id}/presets")
+                .Parameter((p) =>
+                {
+                    if (page.HasValue)
+                    {
+                        p.Add(new RestParameter { Key = "page", Value = page });
+                    }
+                    if (per_page.HasValue)
+                    {
+                        p.Add(new RestParameter { Key = "per_page", Value = per_page });
+                    }
+                });
+
+            var result = root.Get<Pagination<Presets>>();
+
+            if (result.Content.Paging?.Next != null)
+            {
+                result.Content.NextAction = () => RootAuthorization().Command(result.Content.Paging.Next).Get<Pagination<Presets>>();
+            }
+
+            if (result.Content.Paging?.Previous != null)
+            {
+                result.Content.PreviousAction = () => RootAuthorization().Command(result.Content.Paging.Previous).Get<Pagination<Presets>>();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// This method returns every embed preset that belongs to the authenticated user.
+        /// </summary>
+        /// <param name="page">The page number of the results to show.</param>
+        /// <param name="per_page">The number of items to show on each page of results, up to a maximum of 100.</param>
+        /// <returns></returns>
+        public RestResult<Pagination<Presets>> GetAllTheEmbedPresetsThatAUserHasCreadted(int? page = null, int? per_page = null)
+        {
+            var root = RootAuthorization()
+                .Command($"/me/presets")
+                .Parameter((p) =>
+                {
+                    if (page.HasValue)
+                    {
+                        p.Add(new RestParameter { Key = "page", Value = page });
+                    }
+                    if (per_page.HasValue)
+                    {
+                        p.Add(new RestParameter { Key = "per_page", Value = per_page });
+                    }
+                });
+
+            var result = root.Get<Pagination<Presets>>();
+
+            if (result.Content.Paging?.Next != null)
+            {
+                result.Content.NextAction = () => RootAuthorization().Command(result.Content.Paging.Next).Get<Pagination<Presets>>();
+            }
+
+            if (result.Content.Paging?.Previous != null)
+            {
+                result.Content.PreviousAction = () => RootAuthorization().Command(result.Content.Paging.Previous).Get<Pagination<Presets>>();
+            }
+
+            return result;
+        }
+
         #endregion
 
         #region [ Custom logos ]
