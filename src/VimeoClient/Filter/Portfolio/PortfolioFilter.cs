05d31d2eb8c3f51d2dd46ff9fcab1de2830f5603
Federico Mazzanti <f.mazzanti@vargroup.it>
2020-07-27 12:58:19 +0200

    upgrade rest client to 1.7.0 version

--- src/VimeoClient/Filter/Portfolio/PortfolioFilter.cs
+++ src/VimeoClient/Filter/Portfolio/PortfolioFilter.cs
@@ -66,7 +66,7 @@ namespace VimeoClient.Filter.Portfolio
     }
 
 
-    page Number  The page number of the results to show.
-  per_page Number The number of items to show on each page of results, up to a maximum of 100.
-query String  The search query to use to filter the results.
+//    page Number  The page number of the results to show.
+//  per_page Number The number of items to show on each page of results, up to a maximum of 100.
+//query String  The search query to use to filter the results.
   }
