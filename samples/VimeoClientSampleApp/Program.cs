56be22e61a6aa85229f25a27daa82d6ed2126ce9
Federico Mazzanti <f.mazzanti@adiacent.com>
2022-04-24 21:49:45 +0200

    cleanup

--- samples/VimeoClientSampleApp/Program.cs
+++ samples/VimeoClientSampleApp/Program.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using RestClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VimeoClient;
 
 namespace VimeoClientSampleApp
 {
-    internal class Program
+    public class CustomVimeoClient : VimeoClient.Vimeo
+    {
+        public CustomVimeoClient(VimeoProperties properties)
+            : base(properties)
+        {
+        }
+
+        protected override RestBuilder Root()
+        {
+            return base.Root()
+                .OnStart((e) =>
+                {
+                    Console.WriteLine(e.Url);
+                })
+                .OnPreviewContentRequestAsString((json) =>
+                {
+
+                })
+                .OnPreviewContentResponseAsString((json) =>
+                {
+
+                })
+                .OnCompleted((e) =>
+                {
+                    Console.WriteLine("OnCompleted");
+                });
+        }
+    }
+
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var csv = new TinyCsv.TinyCsv<VimeoKey>(options =>
+            {
+                options.HasHeaderRecord = true;
+                options.Delimiter = ";";
+                options.Columns.AddColumn(x => x.AccessToken);
+                options.Columns.AddColumn(x => x.ClientID);
+                options.Columns.AddColumn(x => x.ClientSecret);
+                options.Columns.AddColumn(x => x.UserToken);
+                options.Columns.AddColumn(x => x.Cert);
+            });
+
+            var vimeoKeys = csv.Load("VimeoKeys.Customer.csv");
+            //var vimeoKeys = csv.Load("VimeoKeys.Test.csv");
+
+            var vimeoKey = vimeoKeys.FirstOrDefault();
+            var vimeoClient = new CustomVimeoClient(new VimeoProperties
+            {
+                AccessToken = vimeoKey.AccessToken,
+                ClientSecret = vimeoKey.ClientSecret,
+                ClientId = vimeoKey.ClientID,
+                ValidCertificates = new List<string>() { vimeoKey.Cert }
+            });
+
+            var allCategories = vimeoClient.Categories.GetAllCategories();
+
+            Console.WriteLine("press enter to exit.");
+            Console.ReadLine();
         }
     }
 }
---
+++ samples/VimeoClientSampleApp/VimeoKey.cs
@@ -0,0 +1,11 @@
+﻿namespace VimeoClientSampleApp
+{
+    public partial class VimeoKey
+    {
+        public string AccessToken { get; set; }
+        public string ClientID { get; set; }
+        public string ClientSecret { get; set; }
+        public string UserToken { get; set; }
+        public string Cert { get; set; }
+    }
+}
